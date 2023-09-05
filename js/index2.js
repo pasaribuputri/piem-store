637b46004b76826b6f7d2ef69fd6127cdd25e724
pasaribuputri <pasaribuputri02@gmail.com>
2023-09-06 06:33:41 +0700

    add web html

---
+++ js/index2.js
@@ -0,0 +1,184 @@
+// const data2 = [
+//   {
+//     id: 1,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 2,
+//     name: "Sepatu Coklat Bagus Dipake bksbvvjklllllllllllllll",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 3,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 4,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 5,
+//     name: "Sepatu Coklat Sama Kaya yang Pertama",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 6,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 7,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 8,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 9,
+//     name: "Sepatu Coklat Sepatu bot ahahhahah",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 10,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 11,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 12,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 13,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 14,
+//     name: "Sepatu Coklat bapak bapak",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 15,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 16,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 17,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 18,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 19,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+//   {
+//     id: 20,
+//     name: "Sepatu Coklat",
+//     price: "120000",
+//     image:
+//       "https://klubpria.com/wp-content/uploads/2018/01/46f3a657f1caa61ea67583e4e669d3bd29fda1aa8ad3687ec7e982a28533d702-768x768.jpg",
+//   },
+// ];
+
+window.addEventListener("load", function () {
+  // console.log('lovyu putri');
+  const options = {
+  method: 'GET',
+  headers: {
+  'X-RapidAPI-Key': 'e99de8dc89msh161170a4fc32953p174152jsn8bf4554dff05',
+  'X-RapidAPI-Host': 'shoes-collections.p.rapidapi.com'
+  }
+  };
+
+  fetch('https://shoes-collections.p.rapidapi.com/shoes', options)
+  .then((Response) => Response.json())
+  .then((res) => {
+  const box = document.querySelector(".box-tampung");
+  const data = res;
+  data.forEach((element) => {
+    const bx = document.createElement("div");
+    const gbr = document.createElement("img");
+    const pp = document.createElement("p");
+    const pri = document.createElement("h2");
+    const ker = document.createElement("img");
+    pp.textContent = element.name + "\n";
+    pri.textContent = "$" + element.price + "\n";
+    gbr.src = element.image;
+    ker.src = "assets/images/shopping-cart.png";
+    ker.addEventListener("click", function () {
+      alert("Dimasukkan Ke Keranjang ✓");
+    });
+    pri.className = "price";
+    pp.className = "title";
+    gbr.className = "gg";
+    bx.className = "cart";
+    ker.className = "shopping";
+    bx.appendChild(gbr);
+    bx.appendChild(pp);
+    bx.appendChild(pri);
+    bx.appendChild(ker);
+    box.appendChild(bx);
+  });
+});
+});
